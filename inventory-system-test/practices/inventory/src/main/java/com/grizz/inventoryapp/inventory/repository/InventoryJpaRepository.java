73ecf5f3dd1cf2df499b40436bc8d3c8151cba9d
leejongwoo <leejongwoo.dev@gmail.com>
2025-05-11 19:46:52 +0900

    [Inventory] DataJpaTest로 Repository 구현하기 - DecreaseStock 테스트 통과

--- inventory-system-test/practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/repository/InventoryJpaRepository.java
+++ inventory-system-test/practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/repository/InventoryJpaRepository.java
@@ -3,15 +3,20 @@ package com.grizz.inventoryapp.inventory.repository;
 import com.grizz.inventoryapp.inventory.repository.entity.InventoryEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface InventoryJpaRepository extends JpaRepository<InventoryEntity, Long> {
     @NotNull Optional<InventoryEntity> findByItemId(@NotNull String itemId);
 
-    @NotNull default Integer decreaseStock(@NotNull String itemId, @NotNull Long quantity) {
-        return -1;
-    }
+    @Modifying
+    @Query("update InventoryEntity i set i.stock = i.stock - :quantity " +
+            "where i.itemId = :itemId")
+    @NotNull Integer decreaseStock(@NotNull @Param("itemId") String itemId,
+                                   @NotNull @Param("quantity") Long quantity);
 
     @NotNull InventoryEntity save(@NotNull InventoryEntity entity);
 }
