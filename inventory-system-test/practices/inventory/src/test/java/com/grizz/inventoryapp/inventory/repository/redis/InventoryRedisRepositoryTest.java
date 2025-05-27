6c86ae93b8ec4ad1d1b66eb49387b320322311d0
leejongwoo <leejongwoo.dev@gmail.com>
2025-05-27 22:18:57 +0900

    [Inventory] DataRedisTest로Redis적용하기 - InventoryRedisRepositoryTest GetStock 테스트 통과

--- inventory-system-test/practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/redis/InventoryRedisRepositoryTest.java
+++ inventory-system-test/practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/redis/InventoryRedisRepositoryTest.java
@@ -1,20 +1,61 @@
 package com.grizz.inventoryapp.inventory.repository.redis;
 
 import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
+@Import(InventoryRedisRepositoryImpl.class)
+@Testcontainers
+@DataRedisTest
 public class InventoryRedisRepositoryTest {
 
+    @Container
+    private static final GenericContainer<?> redisContainer;
+
+    static {
+        redisContainer = new GenericContainer<>("redis:7.2.0")
+                .withExposedPorts(6379);
+        redisContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
+    }
+
+    @Autowired
     InventoryRedisRepository sut;
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
 
     final String existingItemId = "1";
     final String nonExistingItemId = "2";
     final Long stock = 100L;
+
+    @BeforeEach
+    void setUp() {
+        redisTemplate.opsForValue().set(sut.key(existingItemId), stock.toString());
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (redisTemplate.getConnectionFactory() != null) {
+            redisTemplate.getConnectionFactory().getConnection().serverCommands().flushAll(); // 테스트 환경에서 Redis 데이터 초기화
+        }
+    }
 
     @Nested
     class GetStock {
