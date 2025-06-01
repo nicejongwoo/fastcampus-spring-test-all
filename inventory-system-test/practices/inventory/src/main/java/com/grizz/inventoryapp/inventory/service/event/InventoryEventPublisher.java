c743ce234b4d758cac5e337b5d4647cab3bc9a1d
leejongwoo <leejongwoo.dev@gmail.com>
2025-06-01 18:15:49 +0900

    [Inventory] test binder로 메시징 시스템 테스트하기 - InventoryEventPublisherTest InventoryDecreasedEvent 테스트 추가

--- inventory-system-test/practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/service/event/InventoryEventPublisher.java
+++ inventory-system-test/practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/service/event/InventoryEventPublisher.java
@@ -3,5 +3,6 @@ package com.grizz.inventoryapp.inventory.service.event;
 import org.jetbrains.annotations.NotNull;
 
 public interface InventoryEventPublisher {
+    String MESSAGE_KEY = "custom_messageKey";
     void publish(@NotNull InventoryEvent event);
 }
