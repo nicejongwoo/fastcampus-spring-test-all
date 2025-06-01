c4b2554015949b01b6738c10d859da424b90a17b
leejongwoo <leejongwoo.dev@gmail.com>
2025-06-01 21:28:39 +0900

    [Inventory] test binder로 메시징 시스템 테스트하기 - InventoryEventPublisherTest assert 메소드 추가

--- inventory-system-test/practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryEventPublisherTest.java
+++ inventory-system-test/practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/repository/InventoryEventPublisherTest.java
@@ -5,7 +5,6 @@ import com.grizz.inventoryapp.inventory.service.event.InventoryDecreasedEvent;
 import com.grizz.inventoryapp.inventory.service.event.InventoryEvent;
 import com.grizz.inventoryapp.inventory.service.event.InventoryEventPublisher;
 import com.grizz.inventoryapp.inventory.service.event.InventoryUpdatedEvent;
-import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,14 +13,11 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.messaging.Message;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonNode;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.grizz.inventoryapp.test.assertion.Assertions.assertDecreasedEventEquals;
+import static com.grizz.inventoryapp.test.assertion.Assertions.assertUpdatedEventEquals;
 
 @SpringBootTest
 @ImportAutoConfiguration(StreamTestConfig.class)
@@ -32,9 +28,6 @@ public class InventoryEventPublisherTest {
     @Autowired
     OutputDestination outputDestination;
 
-    private final ObjectMapper objectMapper = new ObjectMapper()
-            .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     @Nested
     class InventoryDecreasedEventTest {
@@ -51,17 +44,7 @@ public class InventoryEventPublisherTest {
 
             // then
             final Message<byte[]> result = outputDestination.receive(1000, "inventory-out-0");
-
-            final String payload = new String(result.getPayload());
-            final JsonNode json = objectMapper.readTree(payload);
-            assertEquals("InventoryDecreased", json.get("type").asText());
-            assertEquals(itemId, json.get("item_id").asText());
-            assertEquals(quantity, json.get("quantity").asLong());
-            assertEquals(stock, json.get("stock").asLong());
-
-            final String messageKey = result.getHeaders().get(InventoryEventPublisher.MESSAGE_KEY, String.class);
-            assertEquals(itemId, messageKey);
-
+            assertDecreasedEventEquals(result, itemId, quantity, stock);
         }
     }
 
@@ -79,15 +62,7 @@ public class InventoryEventPublisherTest {
 
             // then
             final Message<byte[]> result = outputDestination.receive(1000, "inventory-out-0");
-
-            final String payload = new String(result.getPayload());
-            final JsonNode json = objectMapper.readTree(payload);
-            assertEquals("InventoryUpdated", json.get("type").asText());
-            assertEquals(itemId, json.get("item_id").asText());
-            assertEquals(stock, json.get("stock").asLong());
-
-            final String messageKey = result.getHeaders().get(InventoryEventPublisher.MESSAGE_KEY, String.class);
-            assertEquals(itemId, messageKey);
+            assertUpdatedEventEquals(result, itemId, stock);
         }
     }
 
