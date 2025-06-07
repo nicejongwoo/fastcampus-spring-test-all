0e762cfdbf7f62f9f70e113b162a1331956488b6
leejongwoo <leejongwoo.dev@gmail.com>
2025-06-07 15:47:39 +0900

    [Inventory] sonarqube로코드품질관리하기 - sonar property 설정

--- inventory-system-test/buildSrc/src/main/kotlin/customs.sonar-conventions.gradle.kts
+++ inventory-system-test/buildSrc/src/main/kotlin/customs.sonar-conventions.gradle.kts
@@ -6,5 +6,15 @@ sonar {
     properties {
         property("sonar.organization", "nicejongwoo")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "src/main/java")
+        property("sonar.tests", "src/test/java")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "build/reports/jacoco/test/jacocoTestReport.xml",
+        )
+        property(
+            "sonar.junit.reportPaths",
+            "build/test-results/test"
+        )
     }
 }
